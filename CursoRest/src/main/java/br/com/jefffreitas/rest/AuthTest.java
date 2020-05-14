c7a8f97cb4004865523b597b2d19d14c04ea04ef
JeffFreitas1 <52967772+JeffFreitas1@users.noreply.github.com>
2020-05-14 12:21:51 -0300

    Atualizações

--- CursoRest/src/main/java/br/com/jefffreitas/rest/AuthTest.java
+++ CursoRest/src/main/java/br/com/jefffreitas/rest/AuthTest.java
@@ -2,12 +2,17 @@ package br.com.jefffreitas.rest;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItem;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.Matchers.*;
 
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 public class AuthTest {
 	
@@ -95,14 +100,61 @@ public class AuthTest {
 	
 	@Test
 	public void deveFazerAutenticacaoComToken() {
-		given()
+		//Login na Api
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("email", "jeferson_dent@hotmail.com");
+		login.put("senha", "jeff1337");
+		//Receber o token
+	String token =	given()
 	    .log().all()
-	.when()
-	    .get("http://barrigarest.wcaquino.me/contas")
-	.then()
+	    .body(login)
+	    .contentType(ContentType.JSON)
+	    .when()
+	    .post("http://barrigarest.wcaquino.me/signin")
+	    .then()
+	    .log().all()
 	    .statusCode(200)
-	
-	;
+	    .extract().path("token")
+	    ;
+	    //Obter as contas
+	      given()
+             .log().all()
+             .header("Authorization", "JWT " + token)
+         .when()
+             .get("http://barrigarest.wcaquino.me/contas")
+         .then()
+             .log().all()
+             .statusCode(200)
+             .body("nome", hasItem("teste do jeff"))
+             ;
 }
-	}
-
+	@Test
+	public void deveAcessarAplicacaoWeb() {
+		//Login
+		 String cookie = given()
+             .log().all()
+             .formParam("email", "jeferson_dent@hotmail.com")
+             .formParam("senha", "jeff133")
+             .contentType(ContentType.URLENC.withCharset("UTF-8"))
+        .when()
+            .post("http://seubarriga.wcaquino.me/logar")
+        .then()
+            .log().all()
+            .statusCode(200)
+            .extract().header("set-cookie");
+         ;
+        cookie =  cookie.split("=")[1].split(";")[0];
+        System.out.println(cookie);
+        ;
+	//obter contas 
+	    given()
+            .log().all()
+            .cookie("connect-sid", cookie)
+       .when()
+           .get("http://seubarriga.wcaquino.me/contas")
+       .then()
+           .log().all()
+           .statusCode(200)
+        ;
+}
+}
